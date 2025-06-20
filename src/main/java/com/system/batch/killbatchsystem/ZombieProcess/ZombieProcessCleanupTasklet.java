3e2111c7ffb37ee2397148670a44581c83bc0fea
yoon yang <ysyang@bistelligence.ai>
2025-06-20 17:44:17 +0900

    first commit

---
+++ src/main/java/com/system/batch/killbatchsystem/ZombieProcess/ZombieProcessCleanupTasklet.java
@@ -0,0 +1,28 @@
+package com.system.batch.killbatchsystem.ZombieProcess;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class ZombieProcessCleanupTasklet implements Tasklet {
+  private final int processesToKill = 10;
+  private int killedProcesses = 0;
+
+  @Override
+  public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+    killedProcesses++;
+    log.info("☠\uFE0F  프로세스 강제 종료... ({}/{})", killedProcesses, processesToKill);
+
+    if(killedProcesses >= processesToKill){
+      log.info("\uD83D\uDC80 시스템 안정화 완료. 모든 좀비 프로세스 제거.");
+      return RepeatStatus.FINISHED;
+    }
+    return RepeatStatus.CONTINUABLE;
+  }
+
+}
